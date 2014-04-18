0e137ffce22581d23434f92c7b7db27259dc47bc
achasse <achasse@asu.edu>
2014-04-18 15:47:19 -0700

    Update IdentifierToken.cpp

--- IdentifierToken.cpp
+++ IdentifierToken.cpp
@@ -6,3 +6,62 @@
 #include "IdentifierToken.h"
 
 
+IdentifierToken::IdentifierToken()
+{
+    
+    setLeftChild(NULL);
+    setRightChild(NULL);
+    list = NULL;
+}
+IdentifierToken::~IdentifierToken()
+{
+   
+    LineNumberList *root = getLineNumberList();
+    LineNumberList *tmp = root;
+    
+    while (root != NULL)
+    {
+        tmp = tmp->getNextLineNumber();
+        delete root;
+        root = tmp;
+    }
+
+}
+void IdentifierToken::setLeftChild(IdentifierToken *tok)
+{
+    this->leftChild = tok;
+}
+IdentifierToken *IdentifierToken::getLeftChild()
+{
+    return this->leftChild;
+}
+void IdentifierToken::setRightChild(IdentifierToken *tok)
+{
+    this->rightChild = tok;
+}
+IdentifierToken *IdentifierToken::getRightChild()
+{
+    return this->rightChild;
+}
+void IdentifierToken::addToLineNumberList(LineNumberList *listItem)
+{
+    LineNumberList *tmp = getLineNumberList();
+    
+    if (tmp == NULL)
+    {
+        list = listItem;
+    }
+    else
+    {
+        while (tmp->getNextLineNumber() != NULL)
+        {
+            tmp = tmp->getNextLineNumber();
+        }
+        tmp->setNextLineNumber(listItem);
+    }
+}
+LineNumberList *IdentiferToken::getLineNumberList()
+{
+    return this->list;
+}
+
