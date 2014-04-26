128ebbb27209af5bed10e3493037e1c4af56ea99
achasse <achasse@asu.edu>
2014-04-26 14:52:40 -0700

    Update IdentifierToken.cpp

--- IdentifierToken.cpp
+++ IdentifierToken.cpp
@@ -81,6 +81,6 @@ LineNumberList *IdentifierToken::getLineNumberList()
 }
 void IdentifierToken::print()
 {
-    sprintf("%-s\n", this->getTokenString().c_str() );
+    cout << this->getTokenString().c_str() << endl;
 }
 
