29cc22529db8d168321d5722658abb99215c8501
dougdliu <dougdliu@gmail.com>
2014-04-26 23:09:47 -0700

    Update Real.cpp

--- Real.cpp
+++ Real.cpp
@@ -1,18 +1,12 @@
 /*
  * This is the Real.cpp file
  */
-
-#include "Real.h"
-
-class Real : public Literal<double>
+Real::Real() : Literal<double>(0.0)
 {
-	Real::Real() : Literal<double>(0.0)
-	{
 
-	}
+}
 
-	void Real::print()
-	{
-		cout << this->getTokenString().c_str() << endl;
-	}
-};
+void Real::print()
+{
+	cout << this->getTokenString().c_str() << endl;
+}
