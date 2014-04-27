0fcba7907b29025c2aea91601cd9cd298a274342
dougdliu <dougdliu@gmail.com>
2014-04-26 21:33:32 -0700

    Update String.h

--- String.h
+++ String.h
@@ -1,21 +1,22 @@
 /*
  * This is the header file for String.cpp
  */
- 
+
 #ifndef __Lab5__String__
 #define __Lab5__String__
 
 #include "Token.h"
 #include "Literal.h"
+#include "Literal.cpp"
 
-class String : public Literal
+class String : public Literal<string>
 {
   public:
-    
-    String() : Literal<string>("");
+
+    String();
+    virtual ~String();
     virtual void print();
-    
+
 };
 
 #endif
-    
