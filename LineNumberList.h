cde85f9347eebddbc9b347f9c285a245a96f57fa
achasse <achasse@asu.edu>
2014-04-27 11:10:13 -0700

    Added top block header comment

--- LineNumberList.h
+++ LineNumberList.h
@@ -1,10 +1,16 @@
-//
-//  LineNumberList.h
-//  Lab4
-//
-//  Created by Bryce Holton on 3/28/14.
-//  Copyright (c) 2014 Bryce Holton. All rights reserved.
-//
+/*
+ *Authors: Aaron Chasse, Jake Borg, Deming Liu
+ *CSE 220 - Lab 5
+ *04/27/2014
+ *
+ *Program Description: LineNumberList.h
+ *Header file for LineNumberList.cpp. Contains class definition, public and private variables and methods.
+ *
+ * Created by Bryce Holton on 3/28/14.
+ *
+*/
+
+
 
 #ifndef __Lab4__LineNumberList__
 #define __Lab4__LineNumberList__
